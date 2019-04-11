29b0166f70ef9c49abb90fad2546a123ffb2d2dd
gopal10 <gopalpanchal10@gmail.com>
2019-04-11 16:23:57 +0530

    msg

---
+++ src/main/java/com/gopal/nerproject/NerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gopal.nerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gopal/nerproject/core/Pipeline.java
@@ -0,0 +1,36 @@
+package com.gopal.nerproject.core;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class Pipeline {
+
+    private static StanfordCoreNLP stanfordCoreNLP;
+    private static Properties properties;
+    private  static String propertiesName="tokenize, ssplit, pos, lemma, ner, parse, sentiment";
+
+    private Pipeline()
+    {
+
+    }
+
+    static {
+        properties = new Properties();
+        properties.setProperty("annotators",propertiesName);
+    }
+
+    @Bean(value = "stanfordCoreNLP")
+    public  static StanfordCoreNLP getInstance()
+    {
+        if(stanfordCoreNLP==null)
+        {
+            stanfordCoreNLP= new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+    }
+
+}
